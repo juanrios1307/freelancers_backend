589751c6458b8cb54b21eb78dd4760189122eb2f
juan rios <juan.rios49@eia.edu.co>
2020-08-24 12:08:57 -0500

    a

--- src/routes/routerUser.js
+++ src/routes/routerUser.js
@@ -4,7 +4,7 @@ const  controlUser=require('../controllers/controllerUsers')
 
 
 route.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin","https://pacific-mesa-58371.herokuapp.com/")
+    res.header("Access-Control-Allow-Origin","https://peaceful-ridge-86113.herokuapp.com/")
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS");
     next()
