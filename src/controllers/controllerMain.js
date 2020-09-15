42d07856b2c80255c49d52c42830e2a50f88127d
juan rios <juan.rios49@eia.edu.co>
2020-09-15 11:17:46 -0500

    request profesiones

--- src/controllers/controllerMain.js
+++ src/controllers/controllerMain.js
@@ -5,18 +5,44 @@ const AnuncesWorks = require("../models/AnuncesWorks");
 
 ControllerMain.obtenerProfesiones = (req, res) =>{
 
-    // Se buscan todas las profesiones
-    Worker.find({},{"profesion":1 ,"_id":0}, function (err, workers) {
-        if (err)
-            // Si se ha producido un error, salimos de la función devolviendo  código http 422
-            return (res.type('json').status(422).send({ status: "error", data: "No se puede procesar la entidad, datos incorrectos!" }));
+    if(req.headers['profesion']){
+        const profesion=req.headers['profesion']
+        console.log(profesion)
 
-        // También podemos devolver así la información:
-        res.status(200).json({
-            status: "ok",
-            data: workers
+
+        Worker.find({profesion}, {"profesion": 1}, function (err, workers) {
+            if (err)
+                // Si se ha producido un error, salimos de la función devolviendo  código http 422
+                return (res.type('json').status(422).send({
+                    status: "error",
+                    data: "No se puede procesar la entidad, datos incorrectos!"
+                }));
+
+            // También podemos devolver así la información:
+            res.status(200).json({
+                status: "ok",
+                data: workers
+            });
         });
-    });
+    }else {
+
+        console.log("No")
+        // Se buscan todas las profesiones
+        Worker.find({}, {"profesion": 1, "_id": 0}, function (err, workers) {
+            if (err)
+                // Si se ha producido un error, salimos de la función devolviendo  código http 422
+                return (res.type('json').status(422).send({
+                    status: "error",
+                    data: "No se puede procesar la entidad, datos incorrectos!"
+                }));
+
+            // También podemos devolver así la información:
+            res.status(200).json({
+                status: "ok",
+                data: workers
+            });
+        });
+    }
 }
 
 ControllerMain.obtenerWorkers =(req,res)=>{
