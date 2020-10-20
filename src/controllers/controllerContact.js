e7e619379270708bc0010ae281ef72a5986e43c2
juan rios <juan.rios49@eia.edu.co>
2020-10-20 18:40:02 -0500

    contactForm

--- src/controllers/controllerContact.js
+++ src/controllers/controllerContact.js
@@ -1,5 +1,9 @@
 const ControllerContact={}
 const Message=require('../models/ContactMessage')
+var nodemailer = require('nodemailer');
+const Worker=require('../models/Worker')
+const User=require('../models/User')
+
 
 ControllerContact.obtener= async (req,res) =>{
 
@@ -21,23 +25,54 @@ ControllerContact.crear= async (req,res)=>{
 
     //Se recibe el id del trabajador buscado
     const worker=req.params.id
-    const user=req.decoded
+    const user=req.decoded.sub
 
     //Se reciben los datos a enviar
-    const mensaje =req.body.mensaje //atributos
+    const {mensaje,asunto} =req.body //atributos
 
-    //se registran los datos en el modelo y se suben a la DB
-    const registro=new Message({
-        mensaje,
-        worker,
-        user:user.sub
-    })
-    await  registro.save()
+    Worker.findById(worker, function (err, worker) {
+        if (err) {
+            // Devolvemos el código HTTP 404, de producto no encontrado por su id.
+            res.status(404).json({ status: "error", data: "No se ha encontrado el worker con id: "+req.params.id});
+        } else {
+            // También podemos devolver así la información:
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'quickservices20202@gmail.com',
+                    pass: '2juan1santiago'
+                }
+            });
 
-    //Se genera respuesta de exito
-    res.json({
-        mensaje:"Registro guardado"
-    })
+            User.findById(worker.user, function (err, user) {
+                if (err) {
+                    // Devolvemos el código HTTP 404, de producto no encontrado por su id.
+                    res.status(404).json({ status: "error", data: "No se ha encontrado el worker con id: "+req.params.id});
+                } else {
+                    var mailOptions = {
+                        from: 'quickservices20202@gmail.com',
+                        to: user.correo,
+                        subject: asunto,
+                        text: mensaje
+                    };
+
+                    transporter.sendMail(mailOptions, function(error, info){
+                        if (error) {
+                            res.status(404).json({ status: "error", data: error});
+                        } else {
+                            res.status(200).json({ status: "ok", data: "Formulario enviado"});
+                        }
+                    });
+                }
+
+            }
+            )
+
+        }
+    }).populate('user')
+
+
+
 }
 
 //Se exporta el controlador
